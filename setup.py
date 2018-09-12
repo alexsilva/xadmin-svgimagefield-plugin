5d825c82cc7962439e0a04c72db24544eb0800b8
alexsilva <geniofuturo@gmail.com>
2018-09-12 19:17:19 -0300

    Add setup and init package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xadmin-svgimagefield-plugin',
+    version='1.0',
+    packages=['xplugin_svgimagefield'],
+    url='https://github.com/alexsilva/xadmin-svgimagefield-plugin',
+    license='MIT',
+    author='alex',
+    author_email='alex@fabricadigital.com.br',
+    description='svgimagefield to django forms.'
+)
