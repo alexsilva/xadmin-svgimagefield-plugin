ccaf7eb22ff25071ce18e6939e4795efe931d915
alexsilva <geniofuturo@gmail.com>
2018-09-12 20:59:15 -0300

    Adding the ability to disable the plugin and exclude support for configured fields.

--- xplugin_svgimagefield/__init__.py
+++ xplugin_svgimagefield/__init__.py
@@ -6,9 +6,15 @@ from .svgimagefield import SVGAndImageFormField
 
 class SVGAndImagePlugin(BaseAdminPlugin):
     """Plugin that adds support for svg images"""
+    has_svgimagefield = True
+    exclude_svgimagefield = ()
+
+    def init_request(self, *args, **kwargs):
+        return self.has_svgimagefield
 
     def formfield_for_dbfield(self, formfield, db_field, **kwargs):
-        if isinstance(formfield, forms.ImageField):
+        if isinstance(formfield, forms.ImageField) and \
+                db_field.name not in self.exclude_svgimagefield:
             attrs = self.admin_view.get_field_attrs(db_field, **kwargs)
             # It inherits from existing classes in order to maintain attributes and styles.
             form_class = attrs.get('form_class', formfield.__class__)
